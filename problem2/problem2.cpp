a9fd8cceca45c9f931d81844af815ea2a35a6218
Stefan Hasselgren <connesy@gmail.com>
2017-06-06 23:53:45 +0200

    Project 2

---
+++ problem2/problem2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+int main(int argc, char const* argv[]) {
+
+	int Old=1;
+	int New=2;
+	int temp;
+	int sum = 0;
+	
+	
+	while (New < 4*pow(10,6))
+	{
+		if (New%2 == 0)
+		{
+			sum += New;
+		}
+		temp = New;
+		New += Old;
+		Old = temp;
+	}
+	
+	std::cout << "Sum: " << sum << "\n";	
+	
+	return 0;
+}

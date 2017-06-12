024b61861520f1bb682db6fc544e769a50599588
Stefan Hasselgren <connesy@gmail.com>
2017-06-12 22:13:11 +0200

    Problem 16 started

---
+++ Problems1_50/problem16/problem16.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+#include <armadillo>
+
+using namespace std;
+using namespace arma;
+
+//vec vec::operator*(double x) const;
+
+int main (int argc, char* argv[]) {
+
+
+	
+	
+
+	return 0;
+}
+
+/*
+vec vec::operator*(double x) const
+{
+	double temp;
+	vec V(this->size());
+	
+	for (int i=0; i<V.size(); i++)
+	{
+		temp = V(i)*x;
+		if (x > 10)
+		{
+			V(i) = 
+*/

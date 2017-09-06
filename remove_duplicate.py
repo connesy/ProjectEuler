f5470a48c1bc5ab83277c02144e501f4a09d7d6d
Stefan Hasselgren <connesy@gmail.com>
2017-09-06 15:51:21 +0200

    Using git for file transfer

---
+++ remove_duplicate.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  6 13:27:35 2017
+
+@author: stefan
+"""
+
+#def remove_duplicates(elements):
+#    return list(set(elements))
+
+#def remove_duplicates(elements):
+#    numbers = []
+#    for x in elements:
+#        if numbers.count(x) == 0:
+#            numbers.append(x)
+#
+#    return numbers
+
+#def remove_duplicates(elements):
+#    numbers = []
+#    for x in elements:
+#        if x not in numbers:
+#            numbers.append(x)
+#
+#    return numbers
+
+#def remove_duplicates(elements):
+#    numbers = [elements[0],]
+#
+#    for i in range(1,len(elements)):
+#        isInNumbers = False
+#        for j in range(0,i):
+#            if elements[i] == elements[j]:
+#                isInNumbers = True
+#                break
+#
+#        if isInNumbers == False:
+#            numbers.append(elements[i])
+#
+#    return numbers

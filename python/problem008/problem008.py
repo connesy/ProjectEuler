dc615ff05531d289f0f59269b947187ca4355f16
Stefan Mejlgaard <connesy@gmail.com>
2022-10-26 20:26:03 +0200

    Move read_data_file function to file_utils.py

--- python/problem008/problem008.py
+++ python/problem008/problem008.py
@@ -4,12 +4,7 @@
 """
 import numpy as np
 from more_itertools import sliding_window
-from python.utils.file_utils import get_file_path
-
-
-def read_data_file(filename: str) -> str:
-    with get_file_path(filename, __file__).open() as file:
-        return "".join(file.read().splitlines())
+from python.utils.file_utils import read_data_file
 
 
 def greatest_adjacent_product(number_str: str, N: int) -> int:
@@ -22,6 +17,6 @@ def greatest_adjacent_product(number_str: str, N: int) -> int:
 
 
 if __name__ == "__main__":
-    number_str = read_data_file("data.txt")
+    number_str = "".join(read_data_file(module_file_path=__file__))
     print(greatest_adjacent_product(number_str, N=4))
     print(greatest_adjacent_product(number_str, N=13))
--- python/utils/file_utils.py
+++ python/utils/file_utils.py
@@ -2,5 +2,12 @@ from pathlib import Path
 
 
 def get_file_path(filename: str, module_file_path: str) -> Path:
-    """Return the absolute path to the file located next to the problem module file"""
+    """Return the absolute path to a filename located next to the problem module file"""
     return Path(module_file_path).resolve().with_name(filename)
+
+
+def read_data_file(*, module_file_path: str) -> list[str]:
+    """Return the contents of the data file located next to the problem module file"""
+    data_filepath = get_file_path("data.txt", module_file_path)
+    with data_filepath.open() as file:
+        return file.read().splitlines()
